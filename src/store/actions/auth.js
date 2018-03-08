bcae6f83c262220d4a4af43f1f9b7f0388ad9541
Camilo Giraldo <camilogiraldo91@gmail.com>
2018-03-07 21:15:32 -0500

    add sing in - singup pages/action/reducers

---
+++ src/store/actions/auth.js
@@ -0,0 +1,65 @@
+import * as actionTypes from "./actionsTypes";
+import axios from "axios";
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START
+  };
+};
+
+export const authSuccess = (token, userId) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    token: token,
+    userId: userId
+  };
+};
+
+export const authFailed = error => {
+  return {
+    type: actionTypes.AUTH_FAILED,
+    error: error
+  };
+};
+
+export const auth = (email, password, isSignup) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    let url =
+      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCa6wwP2tenRwYSFWcJjhL5eRGWNrRwNfU";
+    if (!isSignup) {
+      url =
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCa6wwP2tenRwYSFWcJjhL5eRGWNrRwNfU";
+    }
+    axios
+      .post(url, authData)
+      .then(response => {
+        console.log(response);
+
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
+      })
+      .catch(error => {
+        dispatch(authFailed(error.response.data.error));
+      });
+  };
+};
+
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
+
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
